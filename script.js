5a3f6611e3172f5ac2a944010958e4f1e760fcc1
Ryan Villanueva <ryanueva@gmail.com>
2022-10-31 17:41:54 -0400

    rm extra #generate element

--- script.js
+++ script.js
@@ -145,8 +145,6 @@ function generatePassword(){
     }
     return password;
     }
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
