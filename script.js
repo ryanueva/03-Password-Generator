90d8746850eda55c140e4800616623fa16bfcc7f
Ryan Villanueva <ryanueva@gmail.com>
2022-10-31 23:41:19 -0400

    added comments to functionality in script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var specialChoice;
 // Function for determining password length
 function determineLength() {
     passwordLength = prompt("Choose how many characters for your password from 8-128 characters");
-
+    // statements for choices outside of valid options
     if (passwordLength<8) {
         alert("Please choose a length between 8-128 characters");
         determineLength();
@@ -24,6 +24,7 @@ function determineLength() {
     }else if (isNaN(passwordLength)) {
         alert("Please choose a length between 8-128 characters");
         determineLength();
+    //statement for choices within valid options
     }else{
         alert("The upcoming screens will ask you to choose the types of characters to be included in your password. \nChoosing 'No' for all three choices will result in a password containing only lower case letters.");
     }
@@ -34,11 +35,12 @@ function determineLength() {
 function determineUppercase() {
     uppercaseChoice = prompt("Would you like your password to include uppercase letters? \n(Yes or No)");
     uppercaseChoice = uppercaseChoice.toLowerCase();
-
+    
+    // statement for blank option
     if (uppercaseChoice === null || uppercaseChoice === "") {
         alert("Please choose Yes or No");
         return uppercaseChoice;
-    
+    // statements for choices within valid options
     }else if (uppercaseChoice === "yes" || uppercaseChoice === "y") {
         uppercaseChoice = true;
         return uppercaseChoice;
@@ -46,7 +48,7 @@ function determineUppercase() {
     }else if (uppercaseChoice === "no" || uppercaseChoice === "n") {
         uppercaseChoice = false;
         return uppercaseChoice;
-
+    // statement for choices outside of valid options
     }else {
         alert("Please choose Yes or No");
         determineUppercase(); 
@@ -58,11 +60,11 @@ function determineUppercase() {
 function determineNumber() {
     numChoice = prompt("Would you like your password to include numbers? \n(Yes or No)");
       numChoice = numChoice.toLowerCase();
-  
+      // statement for blank option
       if (numChoice === null || numChoice === "") {
         alert("Please choose Yes or No");
         determineNumber();
-  
+      // statements for choices within valid options
       }else if (numChoice === "yes" || numChoice ==="y") {
         numChoice = true;
         return numChoice;
@@ -70,7 +72,7 @@ function determineNumber() {
       }else if (numChoice === "no" || numChoice ==="n") {
         numChoice = false;
         return numChoice;
-      
+      // statement for choices outside of valid options
       }else {
         alert("Please choose Yes or No");
         determineNumber();
@@ -82,11 +84,11 @@ function determineNumber() {
 function determineSpecial() {
     specialChoice = prompt("Would you like your password to include special characters? \n(Yes or No)");
       specialChoice = specialChoice.toLowerCase();
-  
+      // statement for blank option
       if (specialChoice === null || specialChoice === "") {
         alert("Please choose Yes or No");
         determineSpecial();
-  
+      // statement for choices within valid options
       }else if (specialChoice === "yes" || specialChoice ==="y") {
         specialChoice = true;
         return specialChoice;
@@ -94,7 +96,7 @@ function determineSpecial() {
       }else if (specialChoice === "no" || specialChoice ==="n") {
         specialChoice = false;
         return specialChoice;
-      
+      // statement for choices outside of valid options
       }else {
         alert("Please choose Yes or No");
         determineSpecial();
@@ -115,9 +117,10 @@ function generatePassword() {
 
     var characters = lowercaseChar;
     var password;
+    // statement for yes to all 3 choices
     if (uppercaseChoice && numChoice && specialChoice) {
         characters += uppercaseChar + numChar + specialChar;
-
+    // statements for yes to 2 choices
     }else if (uppercaseChoice && numChoice) {
         characters += uppercaseChar + numChar;
 
@@ -126,7 +129,7 @@ function generatePassword() {
 
     }else if (uppercaseChoice && specialChoice) {
         characters += uppercaseChar + specialChar;
-
+        // statements for yes to 1 choice
     }else if (uppercaseChoice) {
         characters += uppercaseChar;
 
@@ -135,11 +138,11 @@ function generatePassword() {
 
     }else if (specialChoice) {
         characters += specialChar;
-
+    // statement for 0 choices (lowercase only)
     }else{
         characters === lowercaseChar;
     }
-
+    // loops through character choices randomly for the length chosen 
     for(var i = 0; i < passwordLength; i++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
